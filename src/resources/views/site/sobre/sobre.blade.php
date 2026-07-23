697d39ce8fdc8ab52f5efda67474ca6b4a71381b
gCASSYs <gabrielecassiano.ads@gmail.com>
2026-07-23 15:35:19 -0300

    Ainda tentando ajustar as imgs

--- src/resources/views/site/sobre/sobre.blade.php
+++ src/resources/views/site/sobre/sobre.blade.php
@@ -1,16 +1,10 @@
 @extends('layout.site')
 
 @section ('content')
-
-    @include('site.sobre.banner')
-   
+ 
     @include('site.sobre.bem-vindo')
 
     @include('site.sobre.equipe')
-
-    @include('site.sobre.eventos')
-
-    @include('site.sobre.galeria')
 
     @include('site.sobre.depoimento')
 
