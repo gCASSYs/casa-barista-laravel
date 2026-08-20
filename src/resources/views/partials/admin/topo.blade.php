f97bc079515302f53059948a48ac9cb5c97f68a2
gCASSYs <gabrielecassiano.ads@gmail.com>
2026-08-20 17:30:14 -0300

    Estilização do Dash

--- src/resources/views/partials/admin/topo.blade.php
+++ src/resources/views/partials/admin/topo.blade.php
@@ -18,13 +18,13 @@
             <li class="nav-item d-none d-md-block">
               <a href="./index.html" class="nav-link">
                 <i class="bi bi-grid-1x2 me-1" aria-hidden="true"></i>
-                Live preview
+                Pré Visualização
               </a>
             </li>
             <li class="nav-item d-none d-md-block">
               <a href="./docs/introduction.html" class="nav-link">
                 <i class="bi bi-book me-1" aria-hidden="true"></i>
-                Documentation
+                Documentação
               </a>
             </li>
           </ul>
